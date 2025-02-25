01c705a310e53119fcc594a42079297e25f08580
neonew <neonew@gmail.com>
2025-02-25 17:09:11 +0100

    Calculate bits per pixel

--- src/main.rs
+++ src/main.rs
@@ -131,6 +131,10 @@ fn read_divoom_16x16_image<R: Read>(reader: &mut R) -> Result<DynamicImage, Box<
         palette.push(Rgb([red, green, blue]));
     }
 
+    // let bits_per_pixel: u8 = palette.len().log2().ceil();
+    let bits_per_pixel: u8 = f32::log2(palette.len() as f32).ceil() as u8;
+    info!("Color count: {}; Bits per pixel: {}", palette.len(), bits_per_pixel);
+
     Ok(DynamicImage::ImageRgb8(image))
 }
 
