855be3efbd73e8c61c9fac69c08d4b4d3f076c8f
neonew <neonew@gmail.com>
2025-02-27 20:50:39 +0100

    Generalize send(): Allow multiple packets to be sent at once

--- src/lib.rs
+++ src/lib.rs
@@ -6,30 +6,42 @@ use bluetooth_serial_port::{scan_devices, BtAddr, BtProtocol, BtSocket};
 use log::info;
 
 pub async fn list_devices() -> Result<(), Box<dyn Error>> {
-  let duration = Duration::from_secs(20);
-  info!("Scanning bluetooth devices for {:?}", duration);
-  let devices = scan_devices(duration)?;
-  info!("Found bluetooth devices {:?}", devices);
+    let duration = Duration::from_secs(20);
+    info!("Scanning bluetooth devices for {:?}", duration);
+    let devices = scan_devices(duration)?;
+    info!("Found bluetooth devices {:?}", devices);
 
-  Ok(())
+    Ok(())
 }
 
 pub async fn send_command(mac_address: BtAddr) -> Result<(), Box<dyn Error>> {
-  info!("Connecting to device with MAC address {:?}", mac_address);
+    let packet = hex::decode("010d00430000142e000200000028bc0002")?;
+    send(mac_address, &[packet.as_slice()])
+}
 
-  let mut socket = BtSocket::new(BtProtocol::RFCOMM)?;
-  socket.connect(mac_address)?;
-  info!("Connection successful, socket over RFCOMM/SPP acquired");
+fn send(mac_address: BtAddr, packets: &[&[u8]]) -> Result<(), Box<dyn Error>> {
+    info!("Connecting to device with MAC address {:?}", mac_address);
 
-  info!("Sending message..");
-  let message = hex::decode("010d00430000142e000200000028bc0002")?;
-  let num_bytes_written = socket.write(&message)?;
-  info!(
-      "Wrote {}/{} bytes ({}%)",
-      num_bytes_written,
-      message.len(),
-      num_bytes_written * 100 / message.len()
-  );
+    let mut socket = BtSocket::new(BtProtocol::RFCOMM)?;
+    socket.connect(mac_address)?;
+    info!("Connection successful, socket over RFCOMM/SPP acquired");
 
-  Ok(())
+    packets
+        .iter()
+        .enumerate()
+        .try_for_each(|(index, packet)| -> Result<(), Box<dyn Error>> {
+            info!("Sending packet {}/{}..", index + 1, packets.len());
+
+            let num_bytes_written = socket.write(packet)?;
+            info!(
+                "  Wrote {}/{} bytes ({}%)",
+                num_bytes_written,
+                packet.len(),
+                num_bytes_written * 100 / packet.len()
+            );
+
+            Ok(())
+        })?;
+
+    Ok(())
 }
