a50c7cc49bd5e6e47a09ee8fac20870493c622ea
neonew <neonew@gmail.com>
2025-03-01 19:55:46 +0100

    Rewrite in more functional way

--- src/divoom_file_format/mod.rs
+++ src/divoom_file_format/mod.rs
@@ -114,12 +114,11 @@ fn read_divoom_16x16_animation<R: Read>(reader: &mut R) -> Result<Animation, Box
   Ok(Animation::from(frames))
 }
 
-pub fn read_divoom_16x16_animation_from_file(
-  filename: &str
-) -> Result<Animation, Box<dyn Error>> {
-  let file = File::open(filename)?;
-  let mut reader = BufReader::new(file);
-  read_divoom_16x16_animation(&mut reader)
+pub fn read_divoom_16x16_animation_from_file(filename: &str) -> Result<Animation, Box<dyn Error>> {
+  File::open(filename)
+    .map(BufReader::new)
+    .map_err(|e| e.into())
+    .and_then(|mut reader| read_divoom_16x16_animation(&mut reader))
 }
 
 fn _get_palette_from_images(images: &[DynamicImage]) -> IndexSet<Rgb<u8>> {
